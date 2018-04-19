ac30aa8621ee4b13e83118d0ee10a0eea40b598f
Roger Domènech Aguilera <roger.domenech.aguilera@gmail.com>
2018-04-19 18:06:55 +0200

    Recall process, add comments and try to gives 900 likes

--- do_likes.py
+++ do_likes.py
@@ -4,6 +4,7 @@ import csv
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import os
+import random
 import smtplib
 import sys
 
@@ -16,36 +17,73 @@ insta_password = os.environ.get('INSTA_PASSWORD')
 email_username = os.environ.get('EMAIL_USER')
 email_password = os.environ.get('EMAIL_PASSWORD')
 
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+max_likes = 900
+max_retries = 10
+
+users_filename = 'users.csv'
+tags_filename = 'hashtags.csv'
+
+comments = [
+    'Very nice pic @{}, I really love it!',
+    'I love this picture @{}!!!',
+    'What an amazing photo @{}!!',
+    'You have an awesome instagram account! If you want you can check mine...',
+    "I've checked your instagram account and it's awesome!"
+    ]
+
+
+def interact(session, tags):
+
+    friends = read_csv('{}/{}'.format(dir_path, users_filename))
+
+    counter = 0
+    session.set_upper_follower_count(limit=3000)
+    session.set_lower_follower_count(limit=1)
+    session.set_dont_include(friends)
+    session.set_do_comment(enabled=True, percentage=25)
+    session.set_comments(comments, media='Photo')
+
+    for tag in tags:
+        number_of_likes_by_tag = round((max_likes - session.liked_img) / (len(tags) - counter))
+        session.like_by_tags([tag], amount=number_of_likes_by_tag)
+        counter += 1
+
+
+def read_csv(path):
+    with open(path, 'r+') as f:
+        reader = csv.reader(f)
+        return [row[0] for row in reader]
+
+
 def main():
 
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    file_name = 'hashtags.csv'
+    retries_count = 0
 
-    with open('{}/{}'.format(dir_path, file_name), 'r+') as f:
-        reader = csv.reader(f)
-        tags = [row[0] for row in reader]
+    tags = read_csv('{}/{}'.format(dir_path, tags_filename))
 
     session = InstaPy(username=insta_username, password=insta_password, headless_browser=True,
                       multi_logs=False, nogui=True)
-    try:
-        session.login()
 
-        number_of_likes_by_tag = int(900 / len(tags))
-        session.set_upper_follower_count(limit=3000)
-        session.like_by_tags(tags, amount=number_of_likes_by_tag)
-        
-        if session.liked_img < 900:
-                number_of_likes_by_tag = int((900 - session.liked_img) / len(tags))
-                session.like_by_tags(tags, amount=number_of_likes_by_tag)
+    session.login()
     
-        body = 'Number of images liked: {}'.format(session.liked_img)
-        _send_report(body=body)
+    while retries_count < max_retries:
+        try:
+            likes_by_tag = round((max_likes - session.liked_img) / len(tags))
+            if likes_by_tag == 0:
+                pending_likes = round((max_likes - session.liked_img) % len(tags))
+                tag = random.choice(tags)
+                session.like_by_tags([tag], amount=pending_likes)
+                break
 
-    except Exception as exception:
-        _send_report(subject='Something when wrong on Instagramer', body=str(exception))
+            interact(session, tags)
 
-    finally:
-        session.end()
+        except Exception as exception:
+            retries_count += 1
+            # _send_report(subject='Something when wrong on Instagramer', body=str(exception))
+
+    session.end()
 
 
 def _send_report(**kwargs):
@@ -59,7 +97,7 @@ def _send_report(**kwargs):
     body = kwargs.get('body', '')
     msg.attach(MIMEText(body, 'plain'))
      
-    server = smtplib.SMTP('smtp.live.com', 587)
+    server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
     server.login(email_username, email_password)
     text = msg.as_string()
