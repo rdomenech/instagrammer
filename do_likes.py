4b2244771ae1e27d74b9ac33a54f18b1a5907a35
Roger Domènech Aguilera <roger.domenech.aguilera@gmail.com>
2019-08-14 08:31:34 +0000

    Change behaviour to interact with traveller followers

--- do_likes.py
+++ do_likes.py
@@ -26,27 +26,12 @@ max_retries = 40
 users_filename = 'users.csv'
 tags_filename = 'hashtags.csv'
 
-def interact(session, tags, max_likes):
-
-    friends = read_csv('{}/{}'.format(dir_path, users_filename))
-
-    counter = 0
-    session.set_relationship_bounds(
-        enabled=True, potency_ratio=None,
-        delimit_by_numbers=True, max_followers=3000,
-        max_following=100000, min_followers=50, min_following=100)
-    session.set_dont_include(friends)
-
-    for tag in tags:
-        number_of_likes_by_tag = round((max_likes - session.liked_img) / (len(tags) - counter))
-        session.like_by_tags([tag], amount=number_of_likes_by_tag)
-        counter += 1
-
 
 def read_csv(path):
     with open(path, 'r+') as f:
         reader = csv.reader(f)
         return [row[0] for row in reader]
+
 
 def main(max_likes):
 
@@ -54,27 +39,42 @@ def main(max_likes):
 
     retries_count = 0
 
-    tags = read_csv('{}/{}'.format(dir_path, tags_filename))
+    users = ['lamochiladesara', 'juntos_viajando', 'justwotravel', 'viajeroscallejeros', 'traveloveroma', 
+             'viviendodeviaje', 'sofiapozuelo', 'el_viajedetuvida']
 
+    friends = read_csv('{}/{}'.format(dir_path, users_filename))
+    tags = read_csv('{}/{}'.format(dir_path, tags_filename))
     session = InstaPy(username=insta_username, password=insta_password, headless_browser=True,
                       multi_logs=False, nogui=True)
 
     session.login()
     
-    while retries_count < max_retries:
-        try:
-            likes_by_tag = round((max_likes - session.liked_img) / len(tags))
-            if likes_by_tag == 0:
-                pending_likes = round((max_likes - session.liked_img) % len(tags))
-                tag = random.choice(tags)
-                session.like_by_tags([tag], amount=pending_likes)
+    session.set_simulation(enabled=False)
+    #session.set_mandatory_words(tags)
+    session.set_user_interact(amount=1, randomize=True, percentage=50, media='Photo')
+    session.set_do_like(enabled=True, percentage=50)
+    session.set_do_follow(enabled=False, percentage=0)
+    session.set_relationship_bounds(
+        enabled=True, potency_ratio=None,
+        delimit_by_numbers=True, max_followers=3000,
+        max_following=10000, min_followers=20, min_following=100)
+    session.set_dont_include(friends)
+
+    pending_likes = max_likes - session.liked_img 
+
+    try:
+        while pending_likes > 0:
+            number_of_likes_by_user = round(pending_likes / len(users))
+
+            if number_of_likes_by_user == 0:
+                session.interact_user_followers(['de_mayor_quiero_ser_mochilera'], amount=pending_likes, randomize=True)
                 break
-
-            interact(session, tags, max_likes)
-
-        except Exception as exception:
-            print(exception)
-            retries_count += 1
+        
+            session.interact_user_followers(users, amount=number_of_likes_by_user, randomize=True)
+            pending_likes = max_likes - session.liked_img 
+    except Exception as e:
+        print(e)
+        session.end()
 
     session.end()
 
